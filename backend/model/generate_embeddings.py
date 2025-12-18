c77f519af91d5f58919d0ad7a662983d15ad8221
David Milanov <davidwmilanov@gmail.com>
2025-12-18 14:59:22 -0500

    Install and load sentence_transformers and faiss

---
+++ backend/model/generate_embeddings.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import faiss
+
+def generate_embeddings():
+    pass
+
+# Load your cleaned_movies.parquet.
+
+# Download the LLM (it will happen automatically the first time you run it).
+
+# Process all 27,000+ movie descriptions into a high-dimensional vector space.
+
+# Save that space into a .index file.
+
+if __name__ == "__main__":
+    print("running")
